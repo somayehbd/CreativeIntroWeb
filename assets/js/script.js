215907ae319f83bd8a7ac7577458c616eb9afdd2
somayehbd <somayehbd@gmail.com>
2023-03-05 21:32:58 +0100

    Added setInterval to overlay

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,21 @@
-var upBtn = document.getElementById("upBtn")
-var downBtn = document.getElementById("downBtn")
-var overlay = document.querySelector('.overlay')
+let upBtn = document.getElementById("upBtn")
+// let downBtn = document.getElementById("downBtn")
+let overlay = document.querySelector('.overlay')
 
 upBtn.onclick = function () {
-    overlay.style.borderTop="500px solid #ff56bb"
+    overlay.style.borderTop="500px solid #ff56bb";
    
+    let interval = setInterval(function() {
+       
+      overlay.style.borderLeft = "500px solid grey";
+    }, 1000); 
+
+    let interval2 = setInterval(function() {
+      overlay.style.borderBottom = "500px solid black";
+    }, 2000); 
 }
-downBtn.onclick = function () {
-    overlay.style.borderLeft = "500px solid black";
-}
+
+// downBtn.onclick = function () {
+//    alert('bottom arrow')
+    
+// }

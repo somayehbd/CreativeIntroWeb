e7a8e611e241df7f1e8eceeaeda635700e0fed4e
somayehbd <somayehbd@gmail.com>
2023-03-06 15:32:43 +0100

    Feat: changed colors to randomColors

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,15 @@
 let upBtn = document.getElementById("upBtn")
-// let downBtn = document.getElementById("downBtn")
 let overlay = document.querySelector('.overlay')
+let colors = ["#ff56bb","#808080","#8ecdff", "#ef6d00", "#10a37f", "black"];
+
 
 upBtn.onclick = function () {
-    overlay.style.borderTop="500px solid #ff56bb";
-   
+    overlay.style.borderLeft="500px solid #808080";
+    overlay.style.borderBottom="500px solid #8ecdff";
+    
     let interval = setInterval(function() {
-       
-      overlay.style.borderLeft = "500px solid grey";
+      let randomColor = colors[Math.floor(Math.random() * colors.length)];
+      overlay.style.borderTop=`500px solid ${randomColor}`
     }, 1000); 
 
-    let interval2 = setInterval(function() {
-      overlay.style.borderBottom = "500px solid black";
-    }, 2000); 
 }
-
-// downBtn.onclick = function () {
-//    alert('bottom arrow')
-    
-// }

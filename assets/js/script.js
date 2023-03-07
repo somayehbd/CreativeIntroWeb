88d1e313b5d1622264b17788e72681d3ba1784be
somayehbd <somayehbd@gmail.com>
2023-03-07 09:40:04 +0100

    Refactored: select the colors  in order.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,17 @@
-let upBtn = document.getElementById("upBtn")
-let overlay = document.querySelector('.overlay')
-let colors = ["#ff56bb","#808080","#8ecdff", "#ef6d00", "#10a37f", "black"];
-
+let upBtn = document.getElementById("upBtn");
+let overlay = document.querySelector('.overlay');
+let colors = ["#ff56bb", "#808080", "#8ecdff", "#ef6d00", "#10a37f", "black"];
 
 upBtn.onclick = function () {
-    overlay.style.borderLeft="500px solid #808080";
-    overlay.style.borderBottom="500px solid #8ecdff";
-    
-    let interval = setInterval(function() {
-      let randomColor = colors[Math.floor(Math.random() * colors.length)];
-      overlay.style.borderTop=`500px solid ${randomColor}`
-    }, 1000); 
+    let i = 0;
+  setInterval(() => {
+    overlay.style.borderTop = `500px solid ${colors[i]}`;
+    overlay.style.borderLeft = `500px solid ${colors[(i+1) % colors.length]}`;
+    overlay.style.borderBottom = `500px solid ${colors[(i+2) % colors.length]}`;
+    i = (i + 1) % colors.length;
+  }, 3000);
+};
 
-}
+
+
+

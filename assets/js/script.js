7969a3e37877716db6727d9d49b91095b1339dd5
somayehbd <somayehbd@gmail.com>
2023-03-08 17:22:58 +0100

    Feat: created circle

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,15 +3,11 @@ let overlay = document.querySelector('.overlay');
 let colors = ["#ff56bb", "#808080", "#8ecdff", "#ef6d00", "#10a37f", "black"];
 
 upBtn.onclick = function () {
-    let i = 0;
+  let i = 0;
   setInterval(() => {
     overlay.style.borderTop = `500px solid ${colors[i]}`;
-    overlay.style.borderLeft = `500px solid ${colors[(i+1) % colors.length]}`;
-    overlay.style.borderBottom = `500px solid ${colors[(i+2) % colors.length]}`;
+    overlay.style.borderLeft = `500px solid ${colors[(i + 1) % colors.length]}`;
+    overlay.style.borderBottom = `500px solid ${colors[(i + 2) % colors.length]}`;
     i = (i + 1) % colors.length;
   }, 3000);
 };
-
-
-
-

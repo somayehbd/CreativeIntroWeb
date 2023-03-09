b47380e38a86ad8d9b50db87d9f0b4e66855e3c1
somayehbd <somayehbd@gmail.com>
2023-03-09 22:39:32 +0100

    refactor: edit and finaliz

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,20 @@
 let upBtn = document.getElementById("upBtn");
 let overlay = document.querySelector('.overlay');
+let overlayBackground = document.querySelector('.overlayBackground');
 let colors = ["#ff56bb", "#808080", "#8ecdff", "#ef6d00", "#10a37f", "#F0CC00"];
-
-upBtn.onclick = function () {
-  let i = 0;
-  setInterval(() => {
-    overlay.style.borderTop = `500px solid ${colors[i]}`;
-    // overlay.style.borderLeft = `500px solid ${colors[(i + 1) % colors.length]}`;
-     overlay.style.borderRight = `500px solid ${colors[(i + 1) % colors.length]}`;
-    overlay.style.borderBottom = `500px solid ${colors[(i + 2) % colors.length]}`;
-    i = (i + 1) % colors.length;
-  }, 2000);
-};
-
-// down botton event
-let downBtn = document.getElementById('downBtn')
+// rotate the circle element
 let circle = document.getElementById('circle')
 let lastDegree = 0;
 
-downBtn.onclick = function() {
-  lastDegree += 90;
-  circle.style.transform = `rotate(-${lastDegree}deg)`;
+upBtn.onclick = function () {
+  debugger;
+  let i = 0;
+  setInterval(() => {
+    overlayBackground.style.backgroundColor = colors[(i + 1) % colors.length];
+    i = (i + 1) % colors.length;
+    //rotate circle
+    lastDegree += 90;
+    circle.style.transform = `rotate(-${lastDegree}deg)`;
+  }, 2000);
+
 };
